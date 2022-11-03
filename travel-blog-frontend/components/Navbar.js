619dcf31a5e3c75e5c9cb714c8395c0236a3be79
emericgautier <emericgtr@gmail.com>
2022-11-03 12:26:38 +0100

    Navbar structure

--- travel-blog-frontend/components/Navbar.js
+++ travel-blog-frontend/components/Navbar.js
@@ -1,10 +1,19 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../public/travel-logo.png";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className="nav-container">
+      <div className="nav-item-container">
+        <Link href="/">
+          <Image src={logo} alt="Travel Blog Logo" width={140} height={140} />
+        </Link>
+      </div>
+      <div className="nav-item-container">
+        <p>XXXX</p>
+      </div>
+    </nav>
   );
 };
 
--- travel-blog-frontend/pages/_app.js
+++ travel-blog-frontend/pages/_app.js
@@ -1,7 +1,13 @@
+import Navbar from "../components/NavBar";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className="app-container">
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default MyApp;
