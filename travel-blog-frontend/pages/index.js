00a885901609e4a41458c1b516977541fd1e504b
emericgautier <emericgtr@gmail.com>
2022-11-04 13:11:06 +0100

    Update index.js

--- travel-blog-frontend/pages/index.js
+++ travel-blog-frontend/pages/index.js
@@ -1,7 +1,47 @@
-import styles from "../styles/Home.module.css";
+import { getClient } from "../lib/sanity.server";
+import groq from "groq";
+import Head from "next/head";
+import Link from "next/link";
+import Card from "../components/Card";
 
-const Home = () => {
-  return <div></div>;
+const Home = ({ posts }) => {
+  // console.log(posts);
+  return (
+    <div>
+      <Head>
+        <title>Nomad Travel Blog</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <div className="posts-container">
+        {posts?.map((post) => (
+          <Link key={post._id} href="/">
+            <Card post={post} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
+
+export async function getStaticProps({ preview = false }) {
+  const posts = await getClient(preview)
+    .fetch(groq`*[_type == "post" && publishedAt < now()] | order(publishedAt desc) {
+    _id,
+    title,
+    "username": author->username,
+    "categories": categories[]->{id, title},
+    "authorImage": author->avatar,
+    body,
+    mainImage,
+    slug,
+    publishedAt
+  }`);
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 
 export default Home;
