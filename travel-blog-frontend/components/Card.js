4258fc6c0f7f7ff139f896fa74a33257b1b32d51
emericgautier <emericgtr@gmail.com>
2022-11-04 13:11:24 +0100

    Update Card.js

--- travel-blog-frontend/components/Card.js
+++ travel-blog-frontend/components/Card.js
@@ -1,9 +1,33 @@
-import React from "react";
+import { urlFor } from "../lib/sanity";
+import Tag from "./Tag";
 
-const Card = () => {
+const Card = ({ post }) => {
   return (
-    <div>
-      <h1>CARD</h1>
+    <div className="card-container">
+      <h2>{post.title}</h2>
+      <p>Publié le: {new Date(post.publishedAt).toDateString()}</p>
+      <img
+        className="main-image"
+        alt={post.title + " image"}
+        src={urlFor(post.mainImage)}
+      />
+
+      <hr />
+
+      <div className="info-container">
+        <p>Publié par: {post.username}</p>
+        <img
+          className="avatar"
+          alt={post.username + " avatar"}
+          src={urlFor(post.authorImage)}
+        />
+      </div>
+
+      <div className="tag-container">
+        {/* {post.categories.map((category) => (
+          <Tag key={category.id} title={category.title} />
+        ))} */}
+      </div>
     </div>
   );
 };
