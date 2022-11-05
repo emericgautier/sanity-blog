12fe86768a80e8f53a2513e075f34e999ddc450e
emericgautier <emericgtr@gmail.com>
2022-11-05 13:58:06 +0100

    Change logo

--- travel-blog-frontend/components/Navbar.js
+++ travel-blog-frontend/components/Navbar.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "../public/travel-logo.png";
+import logo from "../public/travel-blog-logo.png";
 
 const Navbar = () => {
   return (
     <nav className="nav-container">
       <div className="nav-item-container">
         <Link href="/">
-          <Image src={logo} alt="Travel Blog Logo" width={160} height={140} />
+          <Image src={logo} alt="Travel Blog Logo" width={140} height={140} />
         </Link>
       </div>
       <div className="nav-item-container">
